3e69948ba3c9726517b9b45ac17bd62712425396
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-11-05 13:20:10 +0100

    Add files via upload

---
+++ 13 Dialog result kontrola/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace _13_Dialog_result_kontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxRezultat = new System.Windows.Forms.RichTextBox();
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxRezultat
+            // 
+            this.textBoxRezultat.Location = new System.Drawing.Point(31, 125);
+            this.textBoxRezultat.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxRezultat.Name = "textBoxRezultat";
+            this.textBoxRezultat.Size = new System.Drawing.Size(472, 191);
+            this.textBoxRezultat.TabIndex = 3;
+            this.textBoxRezultat.Text = "";
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(190, 48);
+            this.buttonOtvori.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(118, 25);
+            this.buttonOtvori.TabIndex = 2;
+            this.buttonOtvori.Text = "Otvori MessageBox";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.buttonOtvori_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 368);
+            this.Controls.Add(this.textBoxRezultat);
+            this.Controls.Add(this.buttonOtvori);
+            this.Name = "Form1";
+            this.Text = "Dialog result kontrola";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxRezultat;
+        private System.Windows.Forms.Button buttonOtvori;
+    }
+}
+
---
+++ 13 Dialog result kontrola/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_Dialog_result_kontrola
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOtvori_Click(object sender, EventArgs e)
+        {
+            //korisnik nakon pritiska tipke odlučuje "yes ili no"
+            string poruka = "Odaberite Yes / No / Cancel";
+            string naslov = "Naslovna linija Dijaloga za poruke";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            //korisnikova odluka se ispisuje u TextBox
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme YES";
+
+                    break;
+
+                case DialogResult.No:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme NO";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    textBoxRezultat.Text = "Izabrali ste dugme Cancel";
+
+                    break;
+            }
+        }
+    }
+}
